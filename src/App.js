fa645488bfb1a81afed8306ed815409d04919530
AshrayaEngineering <ashrayaengineering824@outlook.com>
2022-08-05 13:49:10 +0545

    new message

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import './App.css';
+import React, { useState } from "react";
+import Nav from "./components/Navbar/Nav";
+import {Link} from 'react-scroll';
+import {IoMdRocket} from 'react-icons/io';
 
 function App() {
+
+
+  const [visible, setVisible] = useState(false)
+  
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300){
+      setVisible(true)
+    } 
+    else if (scrolled <= 300){
+      setVisible(false)
+    }
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
+
   return (
     <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world! how are you.
-    </h1>
-    <h1 className="text-3xl font-bold underline">
-    Hello world! how are you.
-  </h1>
-  <h1 className="text-3xl font-bold underline">
-  Hello world! how are you.
-</h1>
-<h1 className="text-3xl font-bold underline">
-      Hello world! how are you.
-    </h1>
+      <Link href="#" to="home" spy={true} smooth={true} duration={500} offset={-100}>
+      <button  id="myBtn" style={{display: visible ? 'inline' : 'none'}}
+      class="fixed z-50 bottom-10 right-8 bg-transparent w-20 h-20 drop-shadow-lg flex justify-center items-center text-blue-500 text-6xl hover:drop-shadow-2xl hover:animate-bounce duration-900">
+        <IoMdRocket/></button>
+        </Link>
+        <Nav /*name={data.name} title={data.title} social={data.social}*/ />
     </div>
   );
 }
+
+
 
 export default App;
