a95ddd8252634a0343e719af7672b9d1bcfd74f3
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-08-08 20:09:45 +0545

    added footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Nav from "./Navbar/Nav";
 import {Link} from 'react-scroll';
 import {IoMdRocket} from 'react-icons/io';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
 import Website from "./Link/Website";
 import Mobile from "./Link/Mobile";
 import Desktop from "./Link/Desktop";
@@ -37,32 +37,11 @@ function App() {
       class="fixed z-50 bottom-10 right-8 bg-transparent w-20 h-20 drop-shadow-lg flex justify-center items-center text-blue-500 text-6xl hover:drop-shadow-2xl hover:animate-bounce duration-900">
         <IoMdRocket/></button>
         </Link>
-        <Router>
-  <Nav />
-  <Routes>
-    <Route exact path="/">
-      <Home />
-    </Route>
-    <Route exact path="/Website">
-      <Website />
-    </Route>
-    <Route exact path="/Mobile">
-      <Mobile />
-    </Route>
-    <Route exact path="/Desktop">
-      <Desktop />
-    </Route>
-    <Route exact path="/Iot">
-      <Iot />
-    </Route>
-    <Route exact path="/Contact">
-      <Contact />
-    </Route>
-    <Route exact path="/Career">
-      <Career />
-    </Route>
+        <BrowserRouter>
+        <Routes>
+        <Route exact path="/Home" element={<Home />}/>
   </Routes>
-</Router>
+</BrowserRouter>
     </div>
   );
 }
---
+++ src/Navbar/Footer.js
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return (
+    
+      <div id="skill" className="px-4  py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+     <p>Footer</p>
+      </div>
+    );
+  };
+  
+  export default Footer;
