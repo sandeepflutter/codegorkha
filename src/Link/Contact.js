f7d261decd37c545b56508426e2bd760e8d1552d
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-08-14 22:02:46 +0545

    emailjs added

--- src/Link/Contact.js
+++ src/Link/Contact.js
@@ -1,4 +1,3 @@
-
 import {FaYoutube}from 'react-icons/fa';
 import { FaGithub } from "react-icons/fa";
 import { ImFacebook2 } from "react-icons/im";
@@ -6,10 +5,43 @@ import { FaTwitter } from "react-icons/fa";
 import { FaRegEnvelope } from "react-icons/fa";
 import {FaMedium}from 'react-icons/fa';
 import {FaInstagram}from 'react-icons/fa';
+import React, { useRef, useEffect, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 
 export const Contact = ({social: {youtube, github, medium, facebook, twitter, email, insta}}) => {
+
+  const [status, setStatus] = useState('');
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3xe3saz', 'template_5078hnp', form.current, 'kDfSgnoinmtaetfZf')
+      .then((result) => {
+          console.log(result.text);
+        setStatus('success');
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+    
+  };
+
+  useEffect(() => {
+    if(status === 'success') {
+      setTimeout(() => {
+        setStatus('');
+      }, 3000);
+    }
+  }, [status])
+
+
+
+
     return (
+      <form id="contact" ref={form} onSubmit={sendEmail}>
     <div className='bg-blue-100'>
       <div id="skill" className=" mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-2">
 
@@ -36,19 +68,20 @@ export const Contact = ({social: {youtube, github, medium, facebook, twitter, em
     <div class="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
       <h2 class="text-black text-lg mb-1 font-medium title-font text-center underline">Feedback</h2>
       <p class="text-center mb-5">Never hesitate to contact us.</p>
+      {status && renderAlert()}
       <div class="relative mb-4">
         <label for="name" class="leading-7 text-sm text-black">Name</label>
         <input type="text" id="name" name="name" class="w-full bg-blue-200 rounded border border-blue-300 focus:border-blue-400 focus:ring-2 focus:ring-blue-500 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" spellcheck="false" data-ms-editor="true"/>
       </div>
       <div class="relative mb-4">
         <label for="email" class="leading-7 text-sm text-black">Email</label>
-        <input type="email" id="email" name="email" class="w-full bg-blue-200 rounded border border-blue-300 focus:border-blue-400 focus:ring-2 focus:ring-blue-500 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="email" id="email" name="user_email" class="w-full bg-blue-200 rounded border border-blue-300 focus:border-blue-400 focus:ring-2 focus:ring-blue-500 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
       <div class="relative mb-4">
         <label for="message" class="leading-7 text-sm text-black">Message</label>
         <textarea id="message" name="message" class="w-full bg-blue-200 rounded border border-blue-300 focus:border-blue-400 focus:ring-2 focus:ring-blue-500 h-32 text-base outline-none text-black py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" spellcheck="false" data-ms-editor="true"></textarea>
       </div>
-      <button class="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Send</button>
+      <button type='submit' value="send" class="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Send</button>
       </div>
   </div>
 </section>
@@ -118,9 +151,19 @@ export const Contact = ({social: {youtube, github, medium, facebook, twitter, em
 </div>
       </div>
       </div>
+      </form>
     );
   };
   
+
+
+  const renderAlert = () => (
+    <div className="px-4 py-3 leading-normal text-green-700 bg-blue-100 rounded mb-5 text-center ">
+      <p>Message Submitted successfully</p>
+    </div>)
+
+
+
   export default Contact;
   
   
