345e45c85cd8b77b58b4d73d58528df7993b8c36
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-08-17 18:03:10 +0545

    client review+

--- src/Home/Clients.js
+++ src/Home/Clients.js
@@ -1,5 +1,3 @@
-import logo from '../Asset/logo.png';
-import gp from '../Asset/gp.png';
 import Screen from "../Link/Screen";
 import Marquee from "react-fast-marquee";
 import React from 'react';
@@ -7,62 +5,21 @@ import a from '../Asset/a.jpg';
 
 export const Clients = () => {
 
-    const data = [
-        {
-            family: "AntDesign",
-            names: ["stepforward"]
-          },
-          {
-            family: "Entypo",
-            names: ["500px",]
-          }
-      ];
-
       const img = [
         {
             img: a,
             url: "https://www.ashrayaengineering.com.np/",
           },
-          {
-            img: a,
-            url: "https://www.ashrayaengineering.com.np/",
-          },
-          {
-            img: a,
-            url: "https://www.ashrayaengineering.com.np/",
-          },
-          {
-            img: a,
-            url: "https://www.ashrayaengineering.com.np/",
-          },
-          {
-            img: a,
-            url: "https://www.ashrayaengineering.com.np/",
-          },
-          {
-            img: a,
-            url: "https://www.ashrayaengineering.com.np/",
-          },
-          {
-            img: a,
-            url: "https://www.ashrayaengineering.com.np/",
-          },
-          {
-            img: a,
-            url: "https://www.ashrayaengineering.com.np/",
-          }
       ];
     return (
+      
       <footer class=" bg-sky-900 body-font">
   <div class="">
     <div class="flex flex-col items-center justify-center pb-16">
   <Screen/>
   </div>
-<Marquee>
-</Marquee>
-
-<div>
-  <p className='text-center text-[4vh] text-white pb-2 font-fredericka'>
+<div className='p-10'>
+  <p className='text-center text-[4vh] text-white pb-5 font-fredericka'>
     Our Happy Clients
   </p>
 <Marquee pauseOnHover={true} gradient={false}>       
--- src/Link/Screen.js
+++ src/Link/Screen.js
@@ -10,7 +10,7 @@
         import d from '../Asset/d.jpg';
         
         
-        import { EffectCube } from "swiper";
+        import { Navigation, EffectCube } from "swiper";
         
         
         export const Screen = () => {
@@ -18,26 +18,27 @@
         
               <section id="service">
               <div id="skill" className="px-4  py-10 mx-auto md:max-w-full  md:px-12 lg:px-8 lg:py-10">
-        <div className="flex justify-center items-center">
+        <div className=" justify-center items-center">
+          <p className="text-center text-[4vh] text-white p-5 font-fredericka">Clients Reviews</p>
         <div className="h-72 w-96 text-center">
         <>
               <Swiper
                 effect={"cube"}
                 grabCursor={true}
                 
-                
+  
                 cubeEffect={{
+                  
                   shadow: true,
                   slideShadows: true,
                   shadowOffset: 20,
                   shadowScale: 0.94,
-
-                  loop: true,
-                  speed: 3000,
                 }}
         
                     
-                modules={[EffectCube]}
+                modules={[Navigation, EffectCube]}
+                pagination={true
+                }
                 className="mySwiper"
               >
                 <SwiperSlide>
