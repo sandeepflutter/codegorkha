b5214deb2ebf0a66a733c7c2e9aa1734fe2fd774
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-08-25 19:16:27 +0545

    rive+

--- src/Home/Landing.js
+++ src/Home/Landing.js
@@ -6,27 +6,27 @@ import Rive from 'rive-react';
 export const Landing  = () => {      
     return (
       
-<div class=" bg-blue-900 body-font">
+<div class=" bg-blue-50 body-font">
 <section class="pt-5">
-   <div class="px-3 lg:flex md:flex">
+   <div class="bg-green-200 px-10 lg:flex items-center content-center md:flex lg:justify-between md:justify-between sm:justify-center justify-center">
 
 
-      <div class="flex">
+      <div class="flex bg-purple-200">
             <div class="text-center mx-auto mb-5 lg:mb-5">
-               <h2 class=" text-white font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 font-hahmlet">
+               <h2 class=" text-red-500 font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 font-hahmlet">
                   About
                </h2>
-               <p class="text-base text-body-color font-titillium text-white">
+               <p class="text-base text-body-color font-titillium text-blue-500">
                As a Software company, we provide a wide range of features on the products.
                </p>
             </div>
       </div>
-      
-
-<div className='flex items-center justify-center bg-white full'>
-<div>
-<Rive src={ab} className=""/>
-    </div>
+     
+     
+<div className='flex flex-1 lg:flex-none sm:flex-1 md:flex-none items-center justify-center'>
+<div className='flex items-center justify-center bg-red-200 lg:h-[90vh] md:h-[90vh] h-[60vh]'>
+<Rive src={ab} className="full1 "/>
+</div>
 </div>
 </div>
 </section>
