3c7a0f1408bb1f6ea445399c20bfc302007746c1
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-08-30 08:53:55 +0545

    final

--- src/Link/Tip/Desk.js
+++ src/Link/Tip/Desk.js
@@ -19,43 +19,58 @@ export const Desk  = () => {
     return (
 
 <div class=" bg-red-300 body-font">
-<div class=" bg-white body-font pb-5">
-<section id="service" class="pt-5 lg:pt-[20px] pb-5 lg:pb-[9px] pl-5 pr-5">
-<div class="flex flex-wrap -mx-4">
-         <div class="w-full px-4 py-5">
+
+<div class="">
+
+<div id="about" class=" bg-gradient-to-r from-blue-800 to-blue-700 body-font">
+<section  class="pb-5 lg:pb-[9px] pl-5 pr-5">
+   <div class="px-3 lg:px-20 sm:px-10 md:px-5 lg:flex md:flex">
+
+<div data-aos="fade-right lg:w-1/2 "
+     data-aos-easing="ease-in-sine" className='lg:w-[200vh] md:w-[200vh]'>
+      <div class="flex flex-wrap -mx-4">
+         <div class="w-full px-4">
             <div class="text-center mx-auto mb-5 lg:mb-5">
-               <h2 class=" font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 font-hahmlet">
+               <h2 class=" py-10 text-white font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 font-hahmlet">
                Desktop Application
                </h2>
-               <p class="text-base text-body-color font-titillium">
-               We develop best Desktop Application using modern tools.
-               </p>
             </div>
          </div>
       </div>
- 
-   <div class="lg:flex px-3 lg:px-20 sm:px-10 md:px-5 justify-between">
-     <div>
-     <h1 class="font-hahmlet text-[3vh] text-center">Tech We Work With</h1>
-  <h1 class="font-hahmlet text-[2vh] text-center">FRAMEWORK</h1>
+      
+
+<div className='pb-5 '>
+<p className=' px-5 pb-5 text-white align-middle flex items-center justify-center text-justify font-alegreya text-[2.5vh]'>
+We create desktop applications that are both responsive and suitable for all kind of desktop screen. 
+Using the right technology, we will create specialized applications for your professional and private needs.
+ Applications for desktops are crucial for corporate growth. We deliver you innovative applications that solve your digital problems and boost your 
+ digital presence. Allow us to assist you in growing along with us.
+            </p>
+            <div>
+     <h1 class="font-hahmlet text-[2vh] text-center text-white">Tech We Work With</h1>
+  <h1 class="font-hahmlet text-[1.5vh] text-center text-white">FRAMEWORK</h1>
  <div className='lg:flex text-center relative items-center justify-center md:flex'>
        
- <div class="-m-4 ">
+ <div class="-m-4">
 <div class="p-4 flex items-center justify-center">
-  <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
-  <img src={f} alt="img1" class=" w-10 h-10 mb-1 inline-block"/>
-    <p class="leading-relaxed font-titillium">Flutter</p>
+  <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110 ">
+  <img src={f} alt="img1" class=" w-5 h-5 mb-1 inline-block "/>
+    <p class="leading-relaxed font-titillium text-[2vh] text-white ">Flutter</p>
   </div>
 </div>
 </div>
 </div>
 </div>
-<div className='flex items-center justify-center '>
-<div className='flex items-center justify-center h-[50vh] lg:w-1/2 md:w-1/2 sm:w-1/2  py-5'>
-<Lottie animationData={desk} loop={true} height={200} width={200} />
-</div></div>
+</div>
+
+</div>
+
+<div className='flex items-center justify-center lg:w-1/2 ' >
+<Lottie animationData={desk} loop={true}/>
+</div>
 </div>
 </section>
+</div>
 </div>
 </div>
     )
--- src/Link/Tip/Web.js
+++ src/Link/Tip/Web.js
@@ -24,84 +24,101 @@ export const Web  = () => {
 
       
     return (
-<div class=" bg-white body-font pb-5">
-<section id="service" class="pt-5 lg:pt-[20px] pb-5 lg:pb-[9px] pl-5 pr-5">
-<div class="flex flex-wrap -mx-4">
+<div class="  body-font pb-5">
+<div id="about" class=" bg-gradient-to-r from-blue-800 to-blue-700 body-font">
+<section  class="pb-5 lg:pb-[9px] pl-5 pr-5">
+   <div class="px-3 lg:px-20 sm:px-10 md:px-5 lg:flex md:flex">
+
+<div data-aos="fade-right lg:w-1/2 "
+     data-aos-easing="ease-in-sine" className='lg:w-[200vh] md:w-[200vh]'>
+      <div class="flex flex-wrap -mx-4">
          <div class="w-full px-4">
             <div class="text-center mx-auto mb-5 lg:mb-5">
-               <h2 class=" font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 font-hahmlet">
+               <h2 class=" py-10 text-white font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 font-hahmlet">
                Website
                </h2>
-               <p class="text-base text-body-color font-titillium">
-               We develop best website using modern tools.
-               </p>
             </div>
          </div>
       </div>
- 
-   <div class="lg:flex px-3 lg:px-20 sm:px-10 md:px-5">
-     <div>
-     <h1 class="font-hahmlet text-[3vh] text-center">Tech We Work With</h1>
-  <h1 class="font-hahmlet text-[2vh] text-center">FRONT END</h1>
- <div className='lg:flex text-center relative items-center justify-center md:flex'>
-       
- <div class=" flex flex-wrap -m-4 ">
+      
 
-<div class="p-4 w-1/3">
-<div class=" py-6 rounded-lg transform transition duration-500 hover:scale-110">
-    <AiFillHtml5 class="text-orange-500 w-10 h-10 mb-1 inline-block"/>
-    <p class="leading-relaxed font-titillium">HTML</p>
-  </div>
+<div className='pb-5 '>
+<p className=' px-5 pb-5 text-white align-middle flex items-center justify-center text-justify font-alegreya text-[2.5vh]'>
+We create websites that are both responsive and suitable for all kind of web screen. 
+Using the right technology, we will create specialized applications for your professional and private needs.
+ websites are crucial for business growth. We deliver you innovative applications that solve your digital problems and boost your 
+ digital presence. Allow us to assist you in growing along with us.
+            </p>
+            <div>
+        
+        <div class="flex flex-wrap -mx-4">
+                 
+              </div>
+             <h1 class="font-hahmlet text-[2vh] text-center text-white">Tech We Work With</h1>
+          <h1 class="font-hahmlet text-[1.5vh] text-center text-white">FRONT END</h1>
+         <div className='lg:flex text-center relative items-center justify-center md:flex'>
+               
+         <div class=" flex flex-wrap -m-4 ">
+        
+        <div class="p-4 w-1/3 lg:w-1/6">
+        <div class=" py-6 rounded-lg transform transition duration-500 hover:scale-110">
+            <AiFillHtml5 class="text-orange-500 w-5 h-5 mb-1 inline-block"/>
+            <p class="leading-relaxed font-titillium text-[2vh] text-white">HTML</p>
+          </div>
+        </div>
+        
+        <div class="p-4 w-1/3 lg:w-1/6">
+          <div class="px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
+          <IoLogoCss3 class="text-blue-500 w-5 h-5 mb-1 inline-block"/>
+            <p class="leading-relaxed font-titillium text-[2vh] text-white" >CSS</p>
+          </div>
+        </div>
+        
+        
+        
+        <div class="p-4 w-1/3 lg:w-1/6">
+          <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
+           <DiJavascript class="text-yellow-500 w-5 h-5 mb-1 inline-block"/>
+            <p class="leading-relaxed font-titillium text-[2vh] text-white">JavaScript</p>
+          </div>
+        </div>
+        
+        
+        <div class="p-4 w-1/3 lg:w-1/6">
+          <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
+           <GrReactjs class="text-cyan-500 w-5 h-5 mb-1 inline-block"/>
+            <p class="leading-relaxed font-titillium text-[2vh] text-white">React JS</p>
+          </div>
+        </div>
+        
+        <div class="p-4 w-1/3 lg:w-1/6">
+          <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
+          <SiTailwindcss class="text-sky-500 w-5 h-5 mb-1 inline-block"/>
+            <p class="leading-relaxed font-titillium text-[2vh] text-white">Tailwind CSS</p>
+          </div>
+        </div>
+        
+        
+        
+        <div class="p-4 w-1/3 lg:w-1/6">
+          <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
+          <img src={f} alt="img1" class=" w-5 h-5 mb-1 inline-block"/>
+            <p class="leading-relaxed font-titillium text-[2vh] text-white">Flutter</p>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
 </div>
 
-<div class="p-4 w-1/3">
-  <div class="px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-  <IoLogoCss3 class="text-blue-500 w-10 h-10 mb-1 inline-block"/>
-    <p class="leading-relaxed font-titillium" >CSS</p>
-  </div>
 </div>
 
-
-
-<div class="p-4 w-1/3">
-  <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
-   <DiJavascript class="text-yellow-500 w-10 h-10 mb-1 inline-block"/>
-    <p class="leading-relaxed font-titillium">JavaScript</p>
-  </div>
+<div className='flex items-center justify-center lg:w-1/2 ' >
+<Lottie animationData={web} loop={true}/>
 </div>
-
-
-<div class="p-4 w-1/3">
-  <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
-   <GrReactjs class="text-cyan-500 w-10 h-10 mb-1 inline-block"/>
-    <p class="leading-relaxed font-titillium">React JS</p>
-  </div>
-</div>
-
-<div class="p-4 w-1/3 ">
-  <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
-  <SiTailwindcss class="text-sky-500 w-10 h-10 mb-1 inline-block"/>
-    <p class="leading-relaxed font-titillium">Tailwind CSS</p>
-  </div>
-</div>
-
-
-
-<div class="p-4 w-1/3">
-  <div class="py-6 rounded-lg transform transition duration-500 hover:scale-110">
-  <img src={f} alt="img1" class=" w-10 h-10 mb-1 inline-block"/>
-    <p class="leading-relaxed font-titillium">Flutter</p>
-  </div>
-</div>
-</div>
-</div>
-</div>
-<div className='flex items-center justify-center '>
-<div className='flex items-center justify-center h-[50vh]  pb-5'>
-<Lottie animationData={web} loop={true} height={200} width={200} />
-</div></div>
 </div>
 </section>
+</div>
 </div>
     )
   }
